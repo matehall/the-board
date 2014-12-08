52c1c171c3d545ac80d046ba0084c2ab26dc78af
Markus Tenghall <matehall@gmail.com>
2014-12-08 19:51:53 +0100

    Added files and fortune

--- site/board.js
+++ site/board.js
@@ -1,34 +1,39 @@
 var express = require('express');
 
 var app = express();
+var handlebars = require('express-handlebars').create({defaultlayout:'main'});
 
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+var fortune = require('./lib/fortune.js');
+
+
 app.get('/', function(req, res) {
-    res.type('text/plain');
-    res.send('Board');
+    res.render('home');
 })
 
 app.get('/about', function(req, res) {
-    res.type('text/plain');
-    res.send('About Board');
+        res.render('about',{fortune: fortune.getFortune()});
 })
 
 //custom 404 page
 app.use(function(req, res) {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 app.use(function(err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function() {
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
+
+
+
